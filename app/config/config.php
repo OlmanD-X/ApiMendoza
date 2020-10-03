ba2650e8cb9e9e42e0749f6c5f79cd5910cf6fb9
Olman Quispe <oquisperod@gmail.com>
2020-10-03 05:08:38 -0500

    Create login

--- app/config/config.php
+++ app/config/config.php
@@ -5,37 +5,50 @@
     define("WEB_DEVELOPMENT", true);
 
     define('DB_HOST','localhost');
-    define('DB_USER',"");
+    define('DB_USER',"root");
     define('DB_PASSWORD',"");
-    define('DB_NAME','BSC');
+    define('DB_NAME','bsc');
     define('RUTA_URL','http://localhost/apiMendoza');
 
+    /*Security*/
+	define('SECRETE_KEY', 'OD$$QR**');
+
     // Error Codes
-    define('VALIDATE_PARAMETER_REQUIRED', 	100);
-    define('VALIDATE_PARAMETER_DATATYPE', 	101);
-    define('PARAMETER_IS_INVALID',          102);
-    define('CONTENT_TYPE_NOT_VALID',        103);
-    define('SIZE_FILE_NOT_VALID',           104);
-    define('EXTENSION_FILE_NOT_VALID',      105);
-    define('FILE_IS_NULL',                  106);
-    define('RUC_IS_INVALID',                107);
-    define('EMAIL_IS_INVALID',              108);
-    define('PHONE_IS_INVALID',              109);
-    define('UNIT_IS_INVALID',               110);
-    define('DESC_IS_INVALID',               111);
+    define('REQUEST_METHOD_NOT_VALID',		100);
+    define('VALIDATE_PARAMETER_REQUIRED', 	101);
+    define('VALIDATE_PARAMETER_DATATYPE', 	102);
+    define('PARAMETER_IS_INVALID',          103);
+    define('CONTENT_TYPE_NOT_VALID',        104);
+    define('SIZE_FILE_NOT_VALID',           105);
+    define('EXTENSION_FILE_NOT_VALID',      106);
+    define('FILE_IS_NULL',                  107);
+    define('RUC_IS_INVALID',                108);
+    define('EMAIL_IS_INVALID',              109);
+    define('PHONE_IS_INVALID',              110);
+    define('UNIT_IS_INVALID',               111);
+    define('DESC_IS_INVALID',               112);
+    define('INVALID_USER_PASS', 			113);
+    define('INVALID_ACCESS_TOKEN', 			114);
 
     //Server Errors
-    define('FILE_UPLOAD_NOT_COMPLETE',      300);
-    define('INSERTED_DATA_NOT_COMPLETE',    301);
-    define('UPDATED_DATA_NOT_COMPLETE',     302);
-    define('DELETED_DATA_NOT_COMPLETE',     303);
-    define('GET_DATA_NOT_COMPLETE',         304);
+    define('JWT_PROCESSING_ERROR',			300);
+	define('ATHORIZATION_HEADER_NOT_FOUND',	301);
+    define('ACCESS_TOKEN_ERRORS',			302);
+    define('LOGIN_FAILED',                  303);
+    define('CONNECTION_DATABASE_ERROR',     304);
+    define('FILE_UPLOAD_NOT_COMPLETE',      305);
+    define('INSERTED_DATA_NOT_COMPLETE',    306);
+    define('UPDATED_DATA_NOT_COMPLETE',     307);
+    define('DELETED_DATA_NOT_COMPLETE',     308);
+    define('GET_DATA_NOT_COMPLETE',         309);
+
 
     //Success Codes
-    define('REGISTY_INSERT_SUCCESSFULLY',     200);
-    define('REGISTY_UPDATE_SUCCESSFULLY',     201);
-    define('REGISTY_DELETE_SUCCESSFULLY',     202);
-    define('GET_REGISTIES_SUCCESSFULLY',      203);
+    define('SUCCESS_RESPONSE',                200);
+    define('REGISTY_INSERT_SUCCESSFULLY',     201);
+    define('REGISTY_UPDATE_SUCCESSFULLY',     202);
+    define('REGISTY_DELETE_SUCCESSFULLY',     203);
+    define('GET_REGISTIES_SUCCESSFULLY',      204);
 
     /*Data Type*/
 	define('BOOLEAN', 	'1');
--- app/config/global_functions.php
+++ app/config/global_functions.php
@@ -144,3 +144,31 @@
         }
         return $value;
     }
+
+    function getAuthorizationHeader(){
+        $headers = null;
+        if(isset($_SERVER['Authorization'])){
+            $headers = trim($_SERVER['Authorization']);
+        }
+        else if(isset($_SERVER['HTTP_AUTHORIZATION'])){ //Nginx or fast CGI
+            $headers = trim($_SERVER['HTTP_AUTHORIZATION']);
+        }
+        else if(function_exists('apache_request_headers')){
+            $requesHeaders = apache_request_headers(); //Server side fix for bug in old Android versions (a nice side-effect of this fix means we don't care about capitalization for Authorization)
+            $requesHeaders = array_combine(array_map('ucwords',array_keys($requesHeaders)),array_values($requesHeaders));
+            if(isset($requesHeaders['Authorization'])){
+                $headers = trim($requesHeaders['Authorization']);
+            }
+        }
+        return $headers;
+    }
+
+    function getBearerToken(){
+        $headers = getAuthorizationHeader();
+        if(!empty($headers)){
+            if(preg_match('/Bearer\s(\S+)/',$headers,$matches)){
+                return $matches[1];
+            }
+        }
+        throwError(ATHORIZATION_HEADER_NOT_FOUND,'Access Token Not Found');
+    }
